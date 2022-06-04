43d3fb2abceb9d502ed08c336aab4f3e58dbeab8
Prasad <adiga.prasadns@gmail.com>
2022-06-04 19:55:42 +0530

    feat: home page init

--- sinchu-constructions/src/app/app.module.ts
+++ sinchu-constructions/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
